e734f08b6cd85c5368ef7a4df9eebe92514c4c0b
GarlicB <rockplani@gmail.com>
2023-05-13 02:17:34 +0900

    [FEAT] 페이지 라우팅 때마다 스크롤 최상단으로 이동 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import ScrollToTop from "./components/ScrollToTop";
 import "./main.css";
 
 const root = ReactDOM.createRoot(
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
