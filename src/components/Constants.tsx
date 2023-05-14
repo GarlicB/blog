e560b0188670dcefaac5c8fea243e23937720403
GarlicB <rockplani@gmail.com>
2023-05-14 23:47:48 +0900

    [FEAT] Context + localStorage로 다크모드속성 유지 가능

---
+++ src/components/Constants.tsx
@@ -0,0 +1,6 @@
+export const MAIN_LIGHT_CSS = "./css/main-light.css";
+export const MAIN_DARK_CSS = "./css/main-dark.css";
+export const MD_LIGHT_CSS =
+  "https://cdn.jsdelivr.net/npm/highlight.js@10.7.2/styles/github.min.css";
+export const MD_DARK_CSS =
+  "https://cdn.jsdelivr.net/npm/highlight.js@10.7.2/styles/atom-one-dark.min.css";
--- src/components/Context.tsx
+++ src/components/Context.tsx
@@ -1,5 +1,10 @@
-// Context.tsx
 import { createContext, useState, useEffect, ReactNode } from "react";
+import {
+  MAIN_LIGHT_CSS,
+  MAIN_DARK_CSS,
+  MD_LIGHT_CSS,
+  MD_DARK_CSS,
+} from "./Constants";
 
 type ContextType = {
   isDarkMode: boolean;
@@ -18,40 +23,44 @@ type ContextProviderProps = {
 export const ContextProvider: React.FC<ContextProviderProps> = ({
   children,
 }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const storedDarkMode = localStorage.getItem("isDarkMode"); // 로컬스토리지에서 저장된 값 가져오기
+  const initialDarkMode = storedDarkMode ? JSON.parse(storedDarkMode) : false; // 가져온 값이 있을 경우 파싱, 없을 경우 false로 초기화
+
+  const [isDarkMode, setIsDarkMode] = useState(initialDarkMode);
 
   const toggleTheme = () => {
-    setIsDarkMode((prevIsDarkMode) => !prevIsDarkMode);
+    setIsDarkMode((prevIsDarkMode: boolean) => !prevIsDarkMode);
+  };
+
+  // CSS 링크를 생성하고 추가하는 함수
+  const createCSSLink = (id: string, href: string): HTMLLinkElement => {
+    let link = document.getElementById(id) as HTMLLinkElement;
+
+    if (link) document.head.removeChild(link);
+
+    link = document.createElement("link");
+    link.id = id;
+    link.rel = "stylesheet";
+    link.href = href;
+    document.head.appendChild(link);
+
+    return link;
   };
 
   useEffect(() => {
     // 1. 전체 CSS Light <-> Dark Toggle
-    let link1 = document.getElementById("main-darkmode") as HTMLLinkElement;
-
-    if (link1) {
-      document.head.removeChild(link1);
-    }
-
-    link1 = document.createElement("link");
-    link1.id = "main-darkmode";
-    link1.rel = "stylesheet";
-    link1.href = isDarkMode ? "./css/main-dark.css" : "./css/main-light.css";
-    document.head.appendChild(link1);
+    const link1 = createCSSLink(
+      "main-darkmode",
+      isDarkMode ? MAIN_DARK_CSS : MAIN_LIGHT_CSS
+    );
 
     // 2. Markdown Code Block Light <-> Dark Toggle
-    let link2 = document.getElementById("md-darkmode") as HTMLLinkElement;
+    const link2 = createCSSLink(
+      "md-darkmode",
+      isDarkMode ? MD_DARK_CSS : MD_LIGHT_CSS
+    );
 
-    if (link2) {
-      document.head.removeChild(link2);
-    }
-
-    link2 = document.createElement("link");
-    link2.id = "md-darkmode";
-    link2.rel = "stylesheet";
-    link2.href = isDarkMode
-      ? "https://cdn.jsdelivr.net/npm/highlight.js@10.7.2/styles/atom-one-dark.min.css"
-      : "https://cdn.jsdelivr.net/npm/highlight.js@10.7.2/styles/github.min.css";
-    document.head.appendChild(link2);
+    localStorage.setItem("isDarkMode", JSON.stringify(isDarkMode)); // 값이 변경될 때 로컬스토리지에 저장
 
     return () => {
       document.head.removeChild(link1);
